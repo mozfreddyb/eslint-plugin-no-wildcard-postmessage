dfc113c76de26481e9b91be893261fed5526fd33
Frederik Braun <fbraun+gh@mozilla.com>
2015-11-26 14:48:25 +0100

    Applying gaia coding style, by adding editor-config and re-indenting files (Fixes #1)

--- tests/rules/no-wildcard-postmessage.js
+++ tests/rules/no-wildcard-postmessage.js
@@ -22,52 +22,52 @@ var features = { templateStrings: true, spread: true };
 
 eslintTester.run("no-wildcard-postmessage", rule, {
 
-    valid: [
-        {
-            code: "postMessage(obj);",
-            ecmaFeatures: features
-        },
-        {
-            code: 'frame.postMessage(obj, "http://domain.tld");',
-            ecmaFeatures: features
-        },
-        {
-            code: "frame.postMessage(obj, 'http://domain.tld');",
-            ecmaFeatures: features
-        },
-        {   // iife
-            code: "(function() {})()",
-            ecmaFeatures: features
-        }
+  valid: [
+    {
+      code: "postMessage(obj);",
+      ecmaFeatures: features
+    },
+    {
+      code: 'frame.postMessage(obj, "http://domain.tld");',
+      ecmaFeatures: features
+    },
+    {
+      code: "frame.postMessage(obj, 'http://domain.tld');",
+      ecmaFeatures: features
+    },
+    {   // iife
+      code: "(function() {})()",
+      ecmaFeatures: features
+    }
 
 
-    ],
+  ],
 
-    // Examples of code that should trigger the rule
-    invalid: [
-        {
-            code: "postMessage(obj, '*');",
-            errors: [{
-                message: "Using postMessage() with wildcard targets is not allowed.",
-                type: "CallExpression"
-            }],
-            ecmaFeatures: features
-        },
-        {
-            code: 'postMessage(obj, "*");',
-            errors: [{
-                message: "Using postMessage() with wildcard targets is not allowed.",
-                type: "CallExpression"
-            }],
-            ecmaFeatures: features
-        },
-        {
-            code: 'win.postMessage(obj, "*");',
-            errors: [{
-                message: "Using postMessage() with wildcard targets is not allowed.",
-                type: "CallExpression"
-            }],
-            ecmaFeatures: features
-        }
-    ]
+  // Examples of code that should trigger the rule
+  invalid: [
+    {
+      code: "postMessage(obj, '*');",
+      errors: [{
+        message: "Using postMessage() with wildcard targets is not allowed.",
+        type: "CallExpression"
+      }],
+      ecmaFeatures: features
+    },
+    {
+      code: 'postMessage(obj, "*");',
+      errors: [{
+        message: "Using postMessage() with wildcard targets is not allowed.",
+        type: "CallExpression"
+      }],
+      ecmaFeatures: features
+    },
+    {
+      code: 'win.postMessage(obj, "*");',
+      errors: [{
+        message: "Using postMessage() with wildcard targets is not allowed.",
+        type: "CallExpression"
+      }],
+      ecmaFeatures: features
+    }
+  ]
 });
